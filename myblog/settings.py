2be0a5132ca4bc8903ecb9852931bbe8c4685aa7
tzniceguy <joeshimbi@gmail.com>
2024-11-14 19:11:01 +0300

    adding cors middleware to first priority

--- myblog/settings.py
+++ myblog/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,8 +63,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'myblog.urls'
@@ -146,6 +146,7 @@ REST_FRAMEWORK = {
     ]
 }
 """
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
