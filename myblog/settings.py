87345f41e67e1abc625f66f11a6706907ce490f1
tzniceguy <joeshimbi@gmail.com>
2024-11-17 12:45:12 +0300

    a hope on static files

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6@(h!e%5!ep74fetn($dn4z!qsita3rtisu8vg=y26*_pljhq)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -128,10 +128,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/ 'static'
-STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Additional locations of static files
+STATICFILES_DIRS = [
+]
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
