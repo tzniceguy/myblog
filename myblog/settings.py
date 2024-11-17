5a3aad5677b4f990bb3a8c2444ab02ec4f28d696
tzniceguy <joeshimbi@gmail.com>
2024-11-17 12:48:51 +0300

    debug off

--- myblog/settings.py
+++ myblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6@(h!e%5!ep74fetn($dn4z!qsita3rtisu8vg=y26*_pljhq)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
